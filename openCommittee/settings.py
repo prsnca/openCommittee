f9e941ec57457f5145ca4a9fcc274d8ff304ba83
Yaron Dinur <prsnca@gmail.com>
2016-10-10 20:24:39 +0300

    changed api to read only

--- openCommittee/settings.py
+++ openCommittee/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = (
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly'
     ],
     'PAGE_SIZE': 10
 }
